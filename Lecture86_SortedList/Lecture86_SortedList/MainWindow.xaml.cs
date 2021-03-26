25c08ab0fae209baefaaf580890c4b49906ad6d0
wirunchana <wirunchana.pair@gmail.com>
2021-03-27 00:27:15 +0700

    add SotedList

---
+++ Lecture86_SortedList/Lecture86_SortedList/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lecture86_SortedList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SortedList sortedList;
+        public MainWindow()
+        {
+            InitializeComponent();
+            sortedList = new SortedList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            sortedList.Add(int.Parse(keyTxt.Text), valueTxt.Text);
+            ICollection collection = sortedList.Keys;
+            var temp = string.Empty;
+            foreach(int key in collection)
+            {
+               temp += sortedList[key].ToString() + "\n";
+            }
+            MessageBox.Show(temp);
+        }
+    }
+}
